9a39c3090d9ac2ab37914e6c392f330b9801af38
Nie-Mand <chill.out.x19@gmail.com>
2022-08-04 16:55:40 +0100

    added script to deploy to mumbai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-};
+  networks: {
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.MUMBAI_PRIVATE_KEY || ""],
+    },
+  },
+}
 
-export default config;
+export default config
