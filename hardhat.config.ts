9815723dac127c04e385970bd147a3d786291937
Nie-Mand <chill.out.x19@gmail.com>
2022-08-09 10:47:24 +0100

    rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,10 @@ const config: HardhatUserConfig = {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.MUMBAI_PRIVATE_KEY || ""],
     },
+    rinkeby: {
+      url: process.env.RINKBEY_RPC,
+      accounts: [process.env.RINKBEY_PRIVATE_KEY || ""],
+    },
   },
 }
 
